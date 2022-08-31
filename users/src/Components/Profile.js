88bb04358927a3efbaaf13bb8bbabdcfc8448152
= <=>
2022-08-31 16:03:48 +0300

    Using Context API for state management in React.js

---
+++ users/src/Components/Profile.js
@@ -0,0 +1,12 @@
+import { LoginContext } from "../Contexts/LoginContext"
+
+function Profile() {
+  return (
+    <>
+        <h1>Profile</h1>
+        <h2>Username: { username }</h2>
+    </>
+  )
+}
+
+export default Profile
